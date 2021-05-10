f50dc2fe903cbfaa19ae8ec226dff6941e074dc7
Matt Boyd <41444594+mcboyd-bu@users.noreply.github.com>
2021-05-10 08:23:35 -0400

    Update hw_example_node.cpp

--- src/hw_example_node.cpp
+++ src/hw_example_node.cpp
@@ -230,9 +230,11 @@ int main(int argc, char ** argv) {
   
   // Last argument is the default name of the node.
   ros::init(argc, argv, "hw_example_node");
+	
+  printf("Test msg 3, node started...\n");
 
 // //   // First initialize the parts list
-//   Parts_List pl;
+  Parts_List pl;
 
 // //   // Call logical cameras over bins and get all parts
 //   Bin_Parts bp;
